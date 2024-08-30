9dd5315a0f1671bf098bcb0d16a43e15eadf1eb7
Tom Foster <tom@tcpip.uk>
2024-08-30 14:22:19 +0100

    Linting and documentation

--- openaitranslate/__init__.py
+++ openaitranslate/__init__.py
@@ -3,6 +3,7 @@ Initializes the OpenAITranslate plugin for Maubot.
 
 This module imports the OpenAITranslate class and sets up the module's public API.
 """
+
 from .translate import OpenAITranslate
 
 __all__ = ["OpenAITranslate"]
